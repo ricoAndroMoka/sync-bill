52aef82087b4426fcb3e031b6b91d180a2f4f8f6
rico-moka <rico.andropurnomo@mokapos.com>
2018-09-25 18:00:38 +0700

    add strong params

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,11 +1,15 @@
 class StaticPagesController < ApplicationController
-
+	skip_before_action :verify_authenticity_token
 	def index
 		@ed = EventDisplay.all
 	end
 
 	def target
-		ed = EventDisplay.create(params[:data])
-		render nothing: true
+		ed = EventDisplay.create(ed_params)
 	end
+
+	def ed_params
+		params.permit(:outlet_id, :business_id, :token, :type)
+	end
+
 end
