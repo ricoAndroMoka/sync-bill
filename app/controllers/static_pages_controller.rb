3b49fd0d78737be619cf6e637d597e038fd898cd
rico-moka <rico.andropurnomo@mokapos.com>
2019-04-29 18:30:34 +0700

    change return data

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -21,7 +21,7 @@ class StaticPagesController < ApplicationController
 				ed.event_name = params[:event_name]
 				ed.save
 				# render nothing: true, status: 200
-				render json: ed, status: 200
+				render json: {}, status: 200
 			end
 		rescue
 			# render nothing: true, status: 500
